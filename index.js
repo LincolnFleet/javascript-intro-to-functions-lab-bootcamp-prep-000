ffd7402b6cbf3798257ad72b75b64ae5cf0f476f
Travis Martin <travisemerymartin@gmail.com>
2018-10-31 23:31:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (true) { string === string.toUpperCase()
+  if { string === string.toUpperCase()
   return 'YES INDEED!'
 }
-  else if (true) { string === string.toLowerCase()
+  else if { string === string.toLowerCase()
   return 'I can\'t hear you!'
 }
   else { return 'I love you, too!'
