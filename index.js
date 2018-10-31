6c3b27d082fcba321bd457ef0e65791c51e06efe
Travis Martin <travisemerymartin@gmail.com>
2018-10-31 22:50:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var string = "Hi."
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  string.toUpperCase()
+  console.log(string)
+}
+
+function logWhisper(string) {
+  string.toLowerCase()
+  console.log(string)
+}
+
+function sayHiToGrandma(string) {
+  if { '${string}' === string.toLowerCase()
+  console.log("I can't hear you!") }
+  elseif { '${string}' === string.toUpperCase()
+  console.log("YES INDEED!") }
+  else {console.log("I love you, too!") }
+}
